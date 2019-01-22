aafcf103939156411abe1e344ef5fa2a9333f94f
Lukasz <oblak7@onet.pl>
2019-01-22 12:18:49 +0100

    Set circle graphics

--- Circle.js
+++ Circle.js
@@ -1,13 +1,44 @@
 class Circle {
-    constructor(x, y, d, angle) {
+    constructor(x, y, d, step_angle, line_dir) {
         this.pos = createVector(x, y);
-        this.r = d;
-        this.angle = angle;
+        this.dotPos = createVector(0, 0);
+        this.r = d / 2;
+        this.angle = 0;
+        this.angle_incr = step_angle;
+        if (line_dir != 'v' && line_dir != 'h') {
+            throw new Error('line_dir must be "v" or "h" for vertical or horizontal line following the dot');
+        } else {
+            this.line_dir = line_dir;
+        }
+
+    }
+
+    rotate() {
+        this.angle += this.angle_incr;
+        this.dotPos.x = this.r * cos(this.angle) + this.pos.x;
+        this.dotPos.y = this.r * sin(this.angle) + this.pos.y;
     }
 
     show() {
+        //circle
         noFill();
-        stroke(255);
-        ellipse(this.pos.x, this.pos.y, this.r);
+        stroke(255, 255 * 0.7);
+        strokeWeight(2);
+        ellipse(this.pos.x, this.pos.y, 2 * this.r);
+
+        //dot
+        fill(255);
+        noStroke();
+        ellipse(this.dotPos.x, this.dotPos.y, 10);
+
+        //lines
+        stroke(255, 255 * 0.5);
+        strokeWeight(0.3);
+        if (this.line_dir == 'v') {
+            line(this.dotPos.x, 0, this.dotPos.x, height);
+        } else if (this.line_dir == 'h') {
+            line(0, this.dotPos.y, width, this.dotPos.y);
+        }
+
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,48 @@
-var circle;
+var row_circles = [];
+var col_circles = [];
 
 function setup() {
-    createCanvas(890, 550);
+    createCanvas(1850, 1000);
+    angleMode(DEGREES);
+    var rows = 8;
+    var cols = 4;
+
+    x = 300;
+    y = 100;
+    r = 100;
+    angleIncrement = 1;
+
+    for (var i = 0; i < rows; i++) {
+        let new_circle = new Circle(x, y, r, angleIncrement, 'v');
+        row_circles[i] = new_circle;;
+        x += 200;
+        angleIncrement += 0.2;
+    }
+
+    x = 100;
+    y = 300;
+    r = 100;
+    angleIncrement = 1;
+
+    for (var i = 0; i < cols; i++) {
+        let new_circle = new Circle(x, y, r, angleIncrement, 'h');
+        col_circles[i] = new_circle;;
+        y += 200;
+        angleIncrement += 0.2;
+    }
+
 }
 
 function draw() {
     background(30);
+
+    for (var i = 0; i < row_circles.length; i++) {
+        row_circles[i].rotate();
+        row_circles[i].show();
+    }
+
+    for (var i = 0; i < col_circles.length; i++) {
+        col_circles[i].rotate();
+        col_circles[i].show();
+    }
 }
